8b40755f393875adc59a08d5475b3287f6bb204b
Александр <ruyearn@gmail.com>
2022-05-04 12:35:12 +0300

    pack useful links in lists

--- scraping.py
+++ scraping.py
@@ -1,6 +1,21 @@
+import requests
 from bs4 import BeautifulSoup
-from test_data import html_doc
 
-soup = BeautifulSoup(html_doc, 'html.parser')
+SITE_LIST = {
+    'Nature': 'https://www.nature.com/news',
+    'Science': 'https://www.science.org/news'
+}
 
-print(soup.prettify())
+nature_response = requests.get(SITE_LIST['Nature'])
+science_response = requests.get(SITE_LIST['Science'])
+
+nature_soup = BeautifulSoup(nature_response.text, 'html.parser')
+science_soup = BeautifulSoup(science_response.text, 'html.parser')
+
+
+all_links = [
+    str(link.get('href')) for link in nature_soup.find_all('a')
+    if str(link.get('href')).startswith('https')
+]
+article_links = [link for link in all_links if 'articles' in link]
+print(article_links)
